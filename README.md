d5394f725bd0695d50d98401edf492c9c0dbe6d0
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-06-30 14:38:00 +0800

    Update Continuation edition's mod name to LL-C

--- README.md
+++ README.md
@@ -1,38 +1,54 @@
-# LibrarianLib
-
-[![Build Status](https://travis-ci.org/TeamWizardry/LibrarianLib.svg?branch=1.12)](https://travis-ci.org/TeamWizardry/LibrarianLib)
+# LibrarianLib-Continuous
 
 ## [Documentation WIP](https://docs.teamwizardry.com/index.php?title=Main_Page)
 
-LibrarianLib is the library mod to end all library mods. It doesn't attempt to do much else than take the pain out of modding. It can handle a great deal of boilerplate code that you really shouldn't have to touch. Its largest components are an automatic serialization system, automatic model creation and registration, a powerful GUI library that takes out the magic numbers and complex control stuctures by making everything modular, and a powerful and flexable particle system.
+LibrarianLib-Continuous is the library mod to end all library mods. 
+It doesn't attempt to do much else than take the pain out of modding. 
+It can handle a great deal of boilerplate code that you really shouldn't have to touch. Its largest components are 
+an automatic serialization system, automatic model creation and registration, a powerful GUI library that takes out 
+the magic numbers and complex control stuctures by making everything modular, and a powerful and flexable particle system.
 
-LibrarianLib depends on [Shadowfacts' Forgelin](https://minecraft.curseforge.com/projects/shadowfacts-forgelin). Make sure you have it if you want to use it.
+LibrarianLib-Continuous depends on [Forgelin-Continuous](https://github.com/ChAoSUnItY/Forgelin-Continuous). 
+Make sure you have it if you want to use it.
 
+## Difference between LibrarianLib and LibrarianLib-Continuous
 
-## Using LibrarianLib as a dev
-Adding LibrarianLib to your dev workspace is easy when using gradle (or maven, or any other similar system relying on maven).
+LibrarianLib is previously developed by TeamWizardry, which depends on outdated Kotlin library bundle provider mod 
+[Shadowfacts' Forgelin](https://minecraft.curseforge.com/projects/shadowfacts-forgelin); while LibrarianLib-Continuous is currently maintained by CleanroomMC, which depends on
+the successor of Shadowfact's Forgelin named [Forgelin-Continuous](https://github.com/ChAoSUnItY/Forgelin-Continuous).
 
-All you need to do is to add `http://maven.bluexin.be/repository/snapshots/` as remote maven repository.
-For example, in gradle, you can add this :
+Please notice that you can replace Shadowfact's Forgelin with Forgelin-Continuous since version 2.0.0.0, meanwhile mods 
+originally depends on LibrarianLib should also compatible with LibrarianLib-Continuous and Forgelin-Continuous.
+
+If you have found any mod that is incompatible with either LibrarianLib-Continuous or Forgelin-Continuous, feel free to 
+report issues in their GitHub repository!
+
+## Using LibrarianLib-Continuous as a dev
+
+### build.gradle
 ```groovy
 repositories {
-  maven {
-    url = "http://maven.bluexin.be/repository/snapshots/"
-  }
-  // Any other repo you may need
+    maven {
+        name = "CleanroomMC"
+        url = uri("https://maven.cleanroommc.com")
+    }
+}
+
+dependencies {
+    compile "com.teamwizardry.librarianlib:LibrarianLib-Continuous-1.12:4.22-2.0"
 }
 ```
 
-Then use the following artifact reference in your dependencies :
+### build.gradle.kts
+```kt
+repositories {
+    maven {
+        name = "CleanroomMC"
+        url = uri("https://maven.cleanroommc.com")
+    }
+}
 
- * group-id: `com.teamwizardry.librarianlib`
- * artifact-id: `librarianlib-$mcversion`
- * version: `$liblibversion-SNAPSHOT`
- * classifier: `deobf` (this will ensure the sources link properly)
-
-For example, this is what a gradle dependency on liblib 4.0 on minecraft 1.12 would look like :
-```groovy
 dependencies {
-  compile "com.teamwizardry.librarianlib:librarianlib-1.12:4.0-SNAPSHOT:deobf"
+    compile("com.teamwizardry.librarianlib:LibrarianLib-Continuous-1.12:4.22-2.0")
 }
 ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,133 +1,53 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val modVersion: String by ext.properties
+val modName: String by ext.properties
+val modGroup: String by ext.properties
+val corePlugin: String by ext.properties
+
 plugins {
     `java-library`
     `maven-publish`
     kotlin("jvm") version("2.0.0")
     id("com.gtnewhorizons.retrofuturagradle") version("1.3.35")
 }
-/*
-val branch = prop("branch") ?: "git rev-parse --abbrev-ref HEAD".execute(rootDir.absolutePath).lines().last()
-logger.info("On branch $branch")
-
-version = "${branch.replace('/', '-')}-".takeUnless { prop("mc_version")?.contains(branch) == true }.orEmpty() + prop("mod_version") + "." + prop("build_number")
-description = "A library for the TeamWizardry mods "
-base.archivesBaseName = prop("mod_name") + "-" + prop("mc_version")
-*/
 
 minecraft {
     mcVersion.set("1.12.2")
-    extraRunJvmArguments.add("-Dfml.coremods.load=${prop("core_plugin")}")
+    extraRunJvmArguments.add("-Dfml.coremods.load=${corePlugin}")
 }
 
-//version = prop("mod_version")
-version = "4.22-2.0"
-description = "A library for the TeamWizardry mods "
-base.archivesBaseName = prop("mod_name") + "-" + minecraft.mcVersion.get()
-
-/*
-minecraft {
-    version = "${prop("mc_version")}-${prop("forge_version")}"
-    mappings = prop("mc_mappings")
-    runDir = "run"
-    coreMod = prop("core_plugin")
-
-    replace("GRADLE:VERSION", prop("mod_version"))
-    replace("GRADLE:BUILD", prop("build_number"))
-    replaceIn("LibrarianLib.kt")
-}
-
-reobf.getByName("jar") {
-    extraLines("PK: com/ibm/icu com/teamwizardry/librarianlib/shade/icu")
-}
-*/
+version = modVersion
+description = "A library for the TeamWizardry mods, continuously maintained by CleanroomMC."
+base.archivesBaseName = modName + "-" + minecraft.mcVersion.get()
 
 sourceSets["main"].allSource.srcDir("src/example/java")
 sourceSets["main"].allSource.srcDir("src/api/java")
 sourceSets["main"].resources.srcDir("src/example/resources")
 
-/*
-val shade by configurations.creating // TODO: investigate contained deps
-
-configurations.implementation.extendsFrom(shade)
-configurations.testImplementation.extendsFrom(shade)
-*/
-
 repositories {
-    maven {
-        name = "Bluexin repo"
-        url = uri("https://maven.bluexin.be/repository/snapshots/")
-    }
     maven {
         name = "CleanroomMC"
         url = uri("https://maven.cleanroommc.com")
     }
-//    maven {
-//        name = "Jitpack.io"
-//        url = uri("https://jitpack.io")
-//    }
 }
 
 dependencies {
-    api("io.github.chaosunity.forgelin:Forgelin-Continuous:1.9.23.0") {
+    api("io.github.chaosunity.forgelin:Forgelin-Continuous:2.0.0.0") {
         isTransitive = false
     }
-    runtimeOnly("io.github.chaosunity.forgelin:Forgelin-Continuous:1.9.23.0") {
+    runtimeOnly("io.github.chaosunity.forgelin:Forgelin-Continuous:2.0.0.0") {
         isTransitive = false
     }
-//    api("net.shadowfacts:Forgelin:1.8.0")
-//    runtime("net.shadowfacts:Forgelin:1.8.0")
-
-//    shade("org.magicwerk:brownies-collections:0.9.13")
-//    implementation("org.magicwerk:brownies-collections:0.9.13")
-
-//    shade("com.ibm.icu:icu4j:63.1")
-//    shade("org.msgpack:msgpack-core:0.8.16")
-//    shade("com.github.thecodewarrior:bitfont:b8251e7ba0")
-//    implementation("com.ibm.icu:icu4j:63.1")
-//    implementation("org.msgpack:msgpack-core:0.8.16")
-//    implementation("com.github.thecodewarrior:bitfont:-SNAPSHOT")
 }
-
-// kotlin.experimental.coroutines = Coroutines.ENABLE
-
-/**
- * Doing this will ensure we get the sources with replaced values
- * as defined in `minecraft` block in our sources jar.
- */
-
-//val sourceJar = tasks.register("sourceJar", Jar::class).apply {
-//    from(
-//            tasks["sourceMainJava"],
-//            tasks["sourceMainKotlin"],
-//            tasks["sourceTestJava"],
-//            tasks["sourceTestKotlin"]
-//    )
-//    include("**/*.kt", "**/*.java", "**/*.scala")
-//    archiveClassifier = "sources"
-//    includeEmptyDirs = false
-//}
 
 tasks {
     getByName<Jar>("jar") {
-//        for (dep in shade) {
-//            from(zipTree(dep)) {
-//                exclude("META-INF", "META-INF/**")
-//                includeEmptyDirs = false
-//                eachFile {
-//                    path = path.replace("com/ibm/icu", "com/teamwizardry/librarianlib/shade/icu")
-//                }
-//                extraLines("PK: com/ibm/icu com/teamwizardry/librarianlib/shade/icu")
-//            }
-//        }
-//        exclude("*/**/librarianlibtest/**", "*/**/librarianlib.test/**")
-//        archiveClassifier = "fat"
-
         manifest {
             attributes(
                 "FMLCorePluginContainsFMLMod" to true,
-                "FMLCorePlugin" to prop("core_plugin"),
+                "FMLCorePlugin" to corePlugin,
             )
         }
     }
@@ -171,127 +91,23 @@ kotlin {
     jvmToolchain(8)
 }
 
-/*
-val dokka by tasks.getting(DokkaTask::class) {
-    outputDirectory = "$buildDir/docs"
-    outputFormat = "javadoc"
-    jdkVersion = 8
-    sourceDirs =
-            tasks["sourceMainJava"].outputs.files +
-            tasks["sourceMainKotlin"].outputs.files +
-            tasks["sourceTestJava"].outputs.files +
-            tasks["sourceTestKotlin"].outputs.files
-
-    includes = listOf("src/dokka/kotlin-dsl.md")
-    doFirst {
-        file(outputDirectory).deleteRecursively()
-    }
-}*/
-
-/*
-val javadocJar by tasks.creating(Jar::class) {
-    from(jar.outputs)
-    archiveClassifier = "javadoc"
-}
-
-val deobfJar by tasks.creating(Jar::class) {
-    from(sourceSets["main"].output)
-}
-
-//val reobfJar : TaskSingleReobf by tasks
-
-lateinit var publication : Publication
 publishing {
-    publication = publications.create("publication", MavenPublication::class) {
-        from(components["java"])
-        artifact(deobf.jar) {
-            builtBy(reobfJar)
-            classifier = "release"
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = modGroup
+            artifactId = modName
+            version = modVersion
+            from(components["java"])
         }
-        artifact(sourceJar)
-        artifact(deobfJar)
-//        artifact(javadocJar)
-        this.artifactId = base.archivesBaseName
     }
-
     repositories {
-        val mavenPassword = if (hasProp("local")) null else prop("mavenPassword")
         maven {
-            val remoteURL = "https://maven.bluexin.be/repository/" + (if ((version as String).contains("SNAPSHOT")) "snapshots" else "releases")
-            val localURL = "file://$buildDir/repo"
-            url = uri(if (mavenPassword != null) remoteURL else localURL)
-            if (mavenPassword != null) {
-                credentials(PasswordCredentials::class.java) {
-                    username = prop("mavenUser")
-                    password = mavenPassword
-                }
+            name = "CleanroomMavenLibrarianLib"
+            url = uri("https://repo.cleanroommc.com/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
     }
 }
-
-bintray {
-    user = prop("bintrayUser")
-    key = prop("bintrayApiKey")
-    publish = true
-    override = true
-    setPublications(publication.name)
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "teamwizardry"
-        name = project.name
-        userOrg = "teamwizardry"
-        websiteUrl = "https://github.com/TeamWizardry/LibrarianLib"
-        githubRepo = "TeamWizardry/LibrarianLib"
-        vcsUrl = "https://github.com/TeamWizardry/LibrarianLib"
-        issueTrackerUrl = "https://github.com/TeamWizardry/LibrarianLib/issues"
-        desc = project.description
-        setLabels("minecraft", "mc", "modding", "forge", "library", "wizardry")
-        setLicenses("LGPL-3.0")
-    })
-}
-
-artifactory {
-    setContextUrl("https://oss.jfrog.org")
-    publish(delegateClosureOf<PublisherConfig> {
-        repository(delegateClosureOf<GroovyObject> {
-            val targetRepoKey = if (project.version.toString().endsWith("-SNAPSHOT")) "oss-snapshot-local" else "oss-release-local"
-            setProperty("repoKey", targetRepoKey)
-            setProperty("username", prop("bintrayUser"))
-            setProperty("password", prop("bintrayApiKey"))
-            setProperty("maven", true)
-        })
-        defaults(delegateClosureOf<GroovyObject> {
-            invokeMethod("publications", publication.name)
-        })
-    })
-}
-
-fun String.execute(wd: String? = null, ignoreExitCode: Boolean = false): String =
-        split(" ").execute(wd, ignoreExitCode)
-
-fun List<String>.execute(wd: String? = null, ignoreExitCode: Boolean = false): String {
-    val process = ProcessBuilder(this)
-            .also { pb -> wd?.let { pb.directory(File(it)) } }
-            .start()
-    var result = ""
-    val errReader = thread { process.errorStream.bufferedReader().forEachLine { logger.error(it) } }
-    val outReader = thread {
-        process.inputStream.bufferedReader().forEachLine { line ->
-            logger.debug(line)
-            result += line
-        }
-    }
-    process.waitFor()
-    outReader.join()
-    errReader.join()
-    if (process.exitValue() != 0 && !ignoreExitCode) error("Non-zero exit status for `$this`")
-    return result
-}
-*/
-fun hasProp(name: String): Boolean = extra.has(name)
-
-fun prop(name: String): String? = extra.properties[name] as? String
-
-
-fun DependencyHandler.coroutine(module: String): Any =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-$module:${prop("coroutinesVersion")}"
